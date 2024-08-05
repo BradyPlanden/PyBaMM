540687de7d71c3dc96b41133ad059747e20737df
Brady Planden <brady.planden@gmail.com>
2024-08-05 19:42:08 +0100

    update benchmark with solution access

--- examples/scripts/compare_solvers.py
+++ examples/scripts/compare_solvers.py
@@ -4,8 +4,8 @@ import time
 
 
 # Create the list of input dicts
-n = 5000  # Number of solves
-multi_inputs = [
+n = 50  # Number of solves
+inputs = [
     {
         "Current function [A]": x,
         "Separator porosity": y,
@@ -15,12 +15,12 @@ multi_inputs = [
 
 # Solve with gradient:
 grad = False
-use_jax = False
+use_jax = True
 
-inputs = {
-    "Current function [A]": 0.222,
-    "Separator porosity": 0.3,
-}
+# inputs = {
+#     "Current function [A]": 0.222,
+#     "Separator porosity": 0.3,
+# }
 
 
 # Function to construct the conventional model and Jax model
@@ -28,11 +28,11 @@ def build_model():
     model = pybamm.lithium_ion.DFN()
     geometry = model.default_geometry
     param = model.default_parameter_values
-    param.update({key: "[input]" for key in inputs.keys()})
+    param.update({key: "[input]" for key in inputs[0].keys()})
     param.process_geometry(geometry)
     param.process_model(model)
     var = pybamm.standard_spatial_vars
-    var_pts = {var.x_n: 20, var.x_s: 20, var.x_p: 20, var.r_n: 10, var.r_p: 10}
+    var_pts = {var.x_n: 10, var.x_s: 10, var.x_p: 10, var.r_n: 10, var.r_p: 10}
     mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
     disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
     disc.process_model(model)
@@ -47,11 +47,11 @@ def build_jax_model():
     jax_model.events = []
     geometry = jax_model.default_geometry
     param = jax_model.default_parameter_values
-    param.update({key: "[input]" for key in inputs.keys()})
+    param.update({key: "[input]" for key in inputs[0].keys()})
     param.process_geometry(geometry)
     param.process_model(jax_model)
     var = pybamm.standard_spatial_vars
-    var_pts = {var.x_n: 20, var.x_s: 20, var.x_p: 20, var.r_n: 10, var.r_p: 10}
+    var_pts = {var.x_n: 10, var.x_s: 10, var.x_p: 10, var.r_n: 10, var.r_p: 10}
     mesh = pybamm.Mesh(geometry, jax_model.default_submesh_types, var_pts)
     disc = pybamm.Discretisation(mesh, jax_model.default_spatial_methods)
     disc.process_model(jax_model)
@@ -60,7 +60,7 @@ def build_jax_model():
 
 
 # Setup
-t_eval = np.linspace(0, 3600, 360)
+t_eval = np.linspace(0, 3600, 7200)
 model = build_model()
 jax_model = build_jax_model()
 
@@ -69,7 +69,7 @@ if pybamm.have_idaklu():
     # Output variables
     output_variables = [
         "Voltage [V]",
-        "Current [A]",
+        # "Current [A]",
         "Time [s]",
     ]
 
@@ -124,135 +124,151 @@ if pybamm.have_jax() and use_jax:
         method="BDF",
     )
 
-# ------ Single solve benchmarks ------ #
-
+# ------ Serial solve benchmarks ------ #
 if pybamm.have_idaklu():
     start_time = time.time()
-    sim = idaklu_solver.solve(
-        model,
-        t_eval,
-        inputs=inputs,
-        calculate_sensitivities=grad,
-    )
+    for i in range(n):
+        sim = idaklu_solver.solve(
+            model,
+            t_eval,
+            inputs=inputs[i],
+            calculate_sensitivities=grad,
+        )
+        V = sim["Voltage [V]"].data
     print(f"Total IDAKLU time per solve: {(time.time() - start_time)}")
 
     start_time = time.time()
-    sim = idaklu_solver_output_vars.solve(
-        model,
-        t_eval,
-        inputs=inputs,
-        calculate_sensitivities=grad,
-    )
+    for i in range(n):
+        sim = idaklu_solver_output_vars.solve(
+            model,
+            t_eval,
+            inputs=inputs[i],
+            calculate_sensitivities=grad,
+        )
+        V = sim["Voltage [V]"].data
     print(f"Total IDAKLU w/ output vars time per solve: {(time.time() - start_time)}")
 
     if ida_jax_solver is not None:
         start_time = time.time()
         # Jax IdaKLU
-        data = f(
-            t_eval,
-            inputs=inputs,
-        )
+        for i in range(n):
+            data = f(
+                t_eval,
+                inputs=inputs[i],
+            )
         print(f"Total Jax IDAKLU time per solve: {(time.time() - start_time)}")
 
 start_time = time.time()
-sim = casadi_solver.solve(
-    model,
-    t_eval,
-    inputs=inputs,
-    calculate_sensitivities=grad,
-)
+for i in range(n):
+    sim = casadi_solver.solve(
+        model,
+        t_eval,
+        inputs=inputs[i],
+        calculate_sensitivities=grad,
+    )
+    V = sim["Voltage [V]"].data
 print(f"Total Casadi time per solve: {(time.time() - start_time)}")  # 0.110s
 
 start_time = time.time()
-sim = casadi_solver_fast.solve(
-    model,
-    t_eval,
-    inputs=inputs,
-    calculate_sensitivities=grad,
-)
+for i in range(n):
+    sim = casadi_solver_fast.solve(
+        model,
+        t_eval,
+        inputs=inputs[i],
+        calculate_sensitivities=grad,
+    )
+    V = sim["Voltage [V]"].data
 print(f"Total Casadi (Fast) time per solve: {(time.time() - start_time)}")  # 0.068s
 
 start_time = time.time()
-sim = casadi_solver_fast_with_events.solve(
-    model,
-    t_eval,
-    inputs=inputs,
-    calculate_sensitivities=grad,
-)
+for i in range(n):
+    sim = casadi_solver_fast_with_events.solve(
+        model,
+        t_eval,
+        inputs=inputs[i],
+        calculate_sensitivities=grad,
+    )
+    V = sim["Voltage [V]"].data
 print(
     f"Total Casadi (Fast with Events) time per solve: {(time.time() - start_time)}"
 )  # 0.0675
 
 if pybamm.have_jax() and use_jax:
     start_time = time.time()
-    jax_solver.solve(
-        jax_model,
-        t_eval,
-        inputs=inputs,
-    )
-    print(f"Total Jax BDF time per solve: {(time.time() - start_time)}")
-
-
-# ------ Multiprocess solve benchmarks ------ #
-# Rebuild model and reconstruct solver
-model = build_model()
-casadi_solver_fast_with_events = pybamm.CasadiSolver(
-    mode="fast with events",
-    rtol=1e-6,
-    atol=1e-6,
-)
-
-# Create the IDAKLU Solver object
-idaklu_solver = pybamm.IDAKLUSolver(
-    rtol=1e-6,
-    atol=1e-6,
-)
-
-# IDAKLU Multi-threaded
-if pybamm.have_idaklu():
-    start_time = time.time()
-    sim = idaklu_solver.solve(
-        model,
-        t_eval,
-        inputs=multi_inputs,
-        calculate_sensitivities=grad,
-    )
-    print(f"Time taken: {time.time() - start_time}")
-
-    # Jax IDAKLU Multi-threaded
-    if pybamm.have_jax() and use_jax:
-        start_time = time.time()
-        sim = ida_jax_solver.solve(
-            model,
+    for i in range(n):
+        sim = jax_solver.solve(
+            jax_model,
             t_eval,
-            inputs=multi_inputs,
-            calculate_sensitivities=grad,
+            inputs=inputs[i],
         )
-        print(f"Total IDA Jax time per solve: {(time.time() - start_time)}")
-        print(f"Mean IDA Jax time per solve: {(time.time() - start_time)/n}")
-
-# Casadi (Fast with Events) Multi-threaded
-start_time = time.time()
-sim = casadi_solver_fast_with_events.solve(
-    model,
-    t_eval,
-    inputs=multi_inputs,
-    calculate_sensitivities=grad,
-)
-print(
-    f"Total Casadi (Fast with Events) time per solve: {(time.time() - start_time)}"
-)  # 192.77s
-print(
-    f"Mean Casadi (Fast with Events) time per solve: {(time.time() - start_time)/n}"
-)  # 0.0385s
-
-# Jax BDF Multi-threaded
-if pybamm.have_jax() and use_jax:
-    start_time = time.time()
-    sim = jax_solver.solve(
-        jax_model,
-        t_eval,
-        inputs=multi_inputs,
-    )
+        V = sim["Voltage [V]"].data
     print(f"Total Jax BDF time per solve: {(time.time() - start_time)}")
-    print(f"Mean Jax BDF time per solve: {(time.time() - start_time)/n}")
+
+
+# # ------ Multiprocess solve benchmarks ------ #
+# # Rebuild model and reconstruct solver
+# model = build_model()
+# casadi_solver_fast_with_events = pybamm.CasadiSolver(
+#     mode="fast with events",
+#     rtol=1e-6,
+#     atol=1e-6,
+# )
+#
+# # Create the IDAKLU Solver object
+# idaklu_solver = pybamm.IDAKLUSolver(
+#     rtol=1e-6,
+#     atol=1e-6,
+# )
+#
+# # IDAKLU Multi-threaded
+# if pybamm.have_idaklu():
+#     start_time = time.time()
+#     sim = idaklu_solver.solve(
+#         model,
+#         t_eval,
+#         inputs=multi_inputs,
+#         calculate_sensitivities=grad,
+#     )
+#     V = sim["Voltage [V]"].data
+#     print(f"Time taken: {time.time() - start_time}")
+#
+#     # Jax IDAKLU Multi-threaded
+#     if pybamm.have_jax() and use_jax:
+#         start_time = time.time()
+#         sim = ida_jax_solver.solve(
+#             model,
+#             t_eval,
+#             inputs=multi_inputs,
+#             calculate_sensitivities=grad,
+#         )
+#         V = sim["Voltage [V]"].data
+#         print(f"Total IDA Jax time per solve: {(time.time() - start_time)}")
+#         print(f"Mean IDA Jax time per solve: {(time.time() - start_time)/n}")
+#
+# # Casadi (Fast with Events) Multi-threaded
+# start_time = time.time()
+# sim = casadi_solver_fast_with_events.solve(
+#     model,
+#     t_eval,
+#     inputs=multi_inputs,
+#     calculate_sensitivities=grad,
+# )
+# V = sim["Voltage [V]"].data
+# print(
+#     f"Total Casadi (Fast with Events) time per solve: {(time.time() - start_time)}"
+# )  # 192.77s
+# print(
+#     f"Mean Casadi (Fast with Events) time per solve: {(time.time() - start_time)/n}"
+# )  # 0.0385s
+#
+# # Jax BDF Multi-threaded
+# if pybamm.have_jax() and use_jax:
+#     start_time = time.time()
+#     sim = jax_solver.solve(
+#         jax_model,
+#         t_eval,
+#         inputs=multi_inputs,
+#     )
+#     V = sim["Voltage [V]"].data
+#     print(f"Total Jax BDF time per solve: {(time.time() - start_time)}")
+#     print(f"Mean Jax BDF time per solve: {(time.time() - start_time)/n}")
